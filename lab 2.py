be25eaa5f4fdc323ee3fbbe7d2d64c55afcb38f7
DmytroAispidoraiSSS <dmitroslab@gmail.com>
2025-09-17 12:52:11 +0300

    Update lab 2.py

--- lab 2.py
+++ lab 2.py
@@ -1,36 +1,24 @@
-# Головний список: 10 int і 10 str
-main_list = [5, 2, 9, 4, 7, 8, 1, 6, 3, 10, "banana", "apple", "kiwi", "grape", "orange", "lemon", "pear", "melon", "plum", "peach"]
+int_list = [5, 2, 8, 4, 3, 7, 9, 1, 10, 6]
+str_list = ["banana", "apple", "kiwi", "grape", "orange", "lemon", "pear", "melon", "peach", "pineapple"]
 
-# Відділяємо числа і строки
-int_list = []
-str_list = []
-
-for elem in main_list:
-    if type(elem) == int:
-        int_list.append(elem)
-    else:
-        str_list.append(elem)
-
-# Сортуємо числа і строки
 int_list.sort()
 str_list.sort()
 
-# Об'єднуємо у новий список: спочатку числа, потім строки
-sorted_list = int_list + str_list
+sorted_list = int_list.copy()
+sorted_list.extend(str_list)
 
-# Знаходимо всі числа, кратні двом
-even_list = []
+sort_list = [ ]
 for num in int_list:
     if num % 2 == 0:
-        even_list.append(num)
+        sort_list.append(num)
 
-# Створюємо список строк у верхньому регістрі
-caps_str_list = []
-for s in str_list:
-    caps_str_list.append(s.upper())
+STR_list = [ ]
+for x in str_list:
+    STR_list.append(x.upper())
 
-# Виводимо всі списки з підписами
-print("Головний список:", main_list)
-print("Відсортований список (int зростання, str від а до я):", sorted_list)
-print("Список чисел, кратних двом:", even_list)
-print("Список строк у верхньому регістрі:", caps_str_list)
+print("стрічковий список:", str_list)
+print("ціло численний список:", int_list)
+print("відсортований список:", sorted_list)
+print("список який кратних двом:", sort_list)
+print("стрічковий список капсом:", STR_list)
+
